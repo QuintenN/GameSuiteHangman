770d1b4e962b0e08e93ec8041f7c103d858d0bc7
STEFAN-DELL\Stefan <stefanverresen@hotmail.com>
2017-05-09 13:14:51 +0200

    Sam denker Stefan doener TekeningTest fix en vorm aangemaakt

---
+++ GamesuiteHangman/src/domain/Vorm.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Vorm {
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+}
