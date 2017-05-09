a881c5a2da5c953d551f2c89d89e6b286ec22573
Robin <robin_lenaerts2011@hotmail.com>
2017-05-09 13:58:19 +0200

    Voegtoe exceptionuitbreiding, doener Robin denker Ruben

--- GamesuiteHangman/src/domain/Tekening.java
+++ GamesuiteHangman/src/domain/Tekening.java
@@ -26,9 +26,43 @@ public class Tekening {
 	}
 	
 	public void voegToe(Vorm vorm){
+		if(vorm instanceof Lijnstuk){
+			Lijnstuk l = (Lijnstuk) vorm;
+			if(l.getStartPunt().getX()<this.getMinX() || l.getStartPunt().getX()>this.getMaxX() || l.getStartPunt().getY()<this.getMinY() || l.getStartPunt().getY()>this.getMaxY()){
+				if(l.getEindPunt().getX()<this.getMinX() || l.getEindPunt().getX()>this.getMaxX() || l.getEindPunt().getY()<this.getMinY() || l.getEindPunt().getY()>this.getMaxY()){
+					throw new DomainException();
+				}
+			}
+		}
+		if(vorm instanceof Driehoek){
+			Driehoek d = (Driehoek) vorm;
+			if(d.getHoekPunt1().getX()<this.getMinX() || d.getHoekPunt1().getX()>this.getMaxX() || d.getHoekPunt1().getY()<this.getMinY() || d.getHoekPunt1().getY()>this.getMaxY()){
+				if(d.getHoekPunt2().getX()<this.getMinX() || d.getHoekPunt2().getX()>this.getMaxX() || d.getHoekPunt2().getY()<this.getMinY() || d.getHoekPunt2().getY()>this.getMaxY()){
+					if(d.getHoekPunt3().getX()<this.getMinX() || d.getHoekPunt3().getX()>this.getMaxX() || d.getHoekPunt3().getY()<this.getMinY() || d.getHoekPunt3().getY()>this.getMaxY()){
+						throw new DomainException();
+					}
+				}
+			}
+		}
 		vormen.add(vorm);
 	}
 	
+	public static int getMinX() {
+		return MIN_X;
+	}
+
+	public static int getMinY() {
+		return MIN_Y;
+	}
+
+	public static int getMaxX() {
+		return MAX_X;
+	}
+
+	public static int getMaxY() {
+		return MAX_Y;
+	}
+
 	public void verwijder(Vorm vorm){
 		vormen.remove(vorm);
 	}
