fa99d9299791ca4dea18f5a1222df5a74d421dc0
Ruben Vannimmen <ruben.vannimmen@gmail.com>
2017-05-12 09:39:24 +0200

    hintwoord test rbnv

---
+++ GamesuiteHangman/test/domain/HintWoord.java
@@ -0,0 +1,56 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class HintWoord {
+	private  ArrayList<HintLetter>woord= new ArrayList<>();
+	private boolean geraden= false;
+	
+
+	public HintWoord(String woord) {
+	setWoord(woord);
+	}
+
+	public String getWoord() {
+		
+		return toString();
+	}
+
+	private void setWoord(String w) {
+		for(int i=0 ; i<w.length(); i++){
+			HintLetter temp =new HintLetter(w.charAt(i));
+			System.out.println(woord);
+			woord.add(temp);
+		}
+	}
+	public boolean raad(char letter){
+		boolean raden= false;
+		for(HintLetter i : woord){
+			if(i.raad(letter) ){
+				raden = true; 
+			}
+		
+		}
+		return raden;
+	}
+
+	public boolean isGeraden() {
+		
+		return this.geraden;
+	}
+
+	private void setGeraden(boolean geraden) {
+		this.geraden = geraden;
+	}
+
+	@Override
+	public String toString() {
+		String result="";
+		for(HintLetter i : woord ){
+			result+="" + i;
+		}
+		return result;
+	}
+	
+
+}
